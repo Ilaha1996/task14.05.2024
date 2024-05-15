138b3b99891482c7db3743c78751564d921d02e1
Ilaha1996 <ilahaah@code.edu.az>
2024-05-15 11:31:19 +0400

    Added tasks

---
+++ PB201taskİlaha/PB201taskİlaha/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PB201taskİlaha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int num = int.Parse(Console.ReadLine()); 
+
+            int length = num.ToString().Length; 
+
+            Console.WriteLine("Number of floors: " + length);
+        }
+    }
+}
---
+++ PrimeAndCompositeNumber/PrimeAndCompositeNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PrimeAndCompositeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 11;
+            int counter = 0;
+
+
+            for (int i = 2; i < num; i++)
+
+            {
+
+                if (num % i == 0)
+                {
+
+                    Console.WriteLine(num + " is a composite number");
+                    counter++;
+                    break;
+                }
+
+            }
+
+            if (counter == 0)
+
+            {
+                Console.WriteLine(num + " is a prime number");
+
+            }
+        }
+    }
+}
